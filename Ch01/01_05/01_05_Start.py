3be35647b16cc73ea928833df5509bf91139951d
Eda AYDIN <edaaydin.ea@outlook.com>
2024-09-22 01:44:12 +0300

    feat: create 01_05_Start.py

---
+++ Ch01/01_05/01_05_Start.py
@@ -0,0 +1,27 @@
+# Minimum and Maximum
+
+playerOneScore = 10
+playerTwoScore = 4
+
+# Find the minimum score between player one and player two
+print(min(playerOneScore, playerTwoScore))
+
+# Find the minimum value among 0, 12, and -19
+print(min(0, 12, -19))
+
+# Find the minimum string lexicographically between "Kathryn" and "Katie"
+print(min("Kathryn", "Katie"))
+
+# Find the minimum string lexicographically between "Angela" and "Bob"
+print(min("Angela", "Bob"))
+
+# Find the maximum score between player one and player two
+print(max(playerOneScore, playerTwoScore))
+
+playerThreeScore = 14
+
+# Find the maximum score among player three, player two, and player one
+print(max(playerThreeScore, playerTwoScore, playerOneScore))
+
+# Find the maximum string lexicographically between "Kathryn" and "Katie"
+print(max("Kathryn", "Katie"))
